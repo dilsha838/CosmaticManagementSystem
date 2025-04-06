75a23976354805f18ce3a68e13ea1c4aa175fca0
Dilshani Samaranayake <dilshanisamaranayake@gmail.com>
2025-04-06 15:52:30 +0530

    Files upload

---
+++ src/cosmaticmanagement/pkg111/CosmaticManagement111.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cosmaticmanagement.pkg111;
+
+/**
+ *
+ * @author WW
+ */
+public class CosmaticManagement111 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
